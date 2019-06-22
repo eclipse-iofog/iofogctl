b663cc80fc7e75305ff6e186ad2cecae145b0146
SergeRadinovich <serge@edgeworx.io>
2019-06-22 17:25:52 +1200

    Fix example for deploy agent command

--- internal/cmd/deploy_agent.go
+++ internal/cmd/deploy_agent.go
@@ -31,7 +31,7 @@ func newDeployAgentCommand() *cobra.Command {
 
 A Controller must first be deployed within the corresponding namespace in order to provision the Agent.`,
 		Example: `iofogctl deploy agent NAME --local
-iofogctl deploy agent NAME --user root --host 32.23.134.3 --key_file ~/.ssh/id_rsa`,
+iofogctl deploy agent NAME --user root --host 32.23.134.3 --key-file ~/.ssh/id_rsa`,
 		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			var err error
