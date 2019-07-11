68227445e96b5e85f4306f9cef22c9f8823c1b9c
SergeRadinovich <serge@edgeworx.io>
2019-07-11 13:52:28 +1200

    Add unit tests for executor factories

---
+++ internal/connect/factory_test.go
@@ -0,0 +1,78 @@
+/*
+ *  *******************************************************************************
+ *  * Copyright (c) 2019 Edgeworx, Inc.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v. 2.0 which is available at
+ *  * http://www.eclipse.org/legal/epl-2.0
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *  *******************************************************************************
+ *
+ */
+
+package connect
+
+import (
+	"github.com/eclipse-iofog/iofogctl/internal/config"
+	"testing"
+)
+
+var baseOpt = Options{
+	Namespace: "default",
+	Email:     "user@domain.com",
+	Password:  "ui89POKJ324!!",
+	Endpoint:  "localhost:51121",
+}
+
+func TestEmptyNamespace(t *testing.T) {
+	opt := baseOpt
+	if _, err := NewExecutor(&opt); err != nil {
+		t.Errorf("Error when using existing namespace default: %s", err.Error())
+	}
+}
+
+func TestKubeFile(t *testing.T) {
+	opt := baseOpt
+	opt.Endpoint = ""
+	opt.KubeFile = "~/.kube/config"
+	if _, err := NewExecutor(&opt); err != nil {
+		t.Errorf("Error when using existing namespace default: %s", err.Error())
+	}
+}
+
+func TestNonExistentNamespace(t *testing.T) {
+	opt := baseOpt
+	opt.Namespace = "connect_factory_test"
+	if _, err := NewExecutor(&opt); err != nil {
+		t.Errorf("Error when using existing namespace default: %s", err.Error())
+	}
+	if _, err := config.GetNamespace(opt.Namespace); err != nil {
+		t.Errorf("Error retrieving namespace: %s", err.Error())
+	}
+}
+
+func TestNoEmail(t *testing.T) {
+	opt := baseOpt
+	opt.Email = ""
+	if _, err := NewExecutor(&opt); err == nil {
+		t.Error("Expected error with no email specified")
+	}
+}
+
+func TestNoPassword(t *testing.T) {
+	opt := baseOpt
+	opt.Password = ""
+	if _, err := NewExecutor(&opt); err == nil {
+		t.Error("Expected error with no password specified")
+	}
+}
+
+func TestNoController(t *testing.T) {
+	opt := baseOpt
+	opt.Endpoint = ""
+	opt.KubeFile = ""
+	if _, err := NewExecutor(&opt); err == nil {
+		t.Error("Expected error with no Controller Kube file or endpoint specified")
+	}
+}
---
+++ internal/deploy/controller/factory_test.go
@@ -0,0 +1,83 @@
+/*
+ *  *******************************************************************************
+ *  * Copyright (c) 2019 Edgeworx, Inc.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v. 2.0 which is available at
+ *  * http://www.eclipse.org/legal/epl-2.0
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *  *******************************************************************************
+ *
+ */
+
+package deploycontroller
+
+import (
+	"github.com/eclipse-iofog/iofogctl/internal/config"
+	"testing"
+)
+
+var baseOpt = Options{
+	Namespace:  "default",
+	KubeConfig: "~/.kube/config",
+	Local:      false,
+}
+
+func TestCreateNamespace(t *testing.T) {
+	if err := config.AddNamespace(baseOpt.Namespace, ""); err != nil {
+		t.Errorf("Error when creating default namespace: %s", err.Error())
+	}
+}
+
+func TestRemote(t *testing.T) {
+	opt := baseOpt
+	if _, err := NewExecutor(&opt); err != nil {
+		t.Errorf("Error when creating remote executor: %s", err.Error())
+	}
+}
+
+func TestLocal(t *testing.T) {
+	opt := baseOpt
+	opt.KubeConfig = ""
+	opt.Host = "localhost"
+	opt.Local = true
+	if _, err := NewExecutor(&opt); err != nil {
+		t.Errorf("Error when creating local executor: %s", err.Error())
+	}
+}
+
+func TestNonExistentNamespace(t *testing.T) {
+	opt := baseOpt
+	opt.Namespace = "non_existent"
+	if _, err := NewExecutor(&opt); err == nil {
+		t.Error("Expected error when non-existent namespace is specified")
+	}
+}
+
+func TestNoHost(t *testing.T) {
+	opt := baseOpt
+	opt.KubeConfig = ""
+	opt.Host = ""
+	if _, err := NewExecutor(&opt); err == nil {
+		t.Error("Expected error when no host is specified")
+	}
+}
+
+func TestNoUser(t *testing.T) {
+	opt := baseOpt
+	opt.KubeConfig = ""
+	opt.User = ""
+	if _, err := NewExecutor(&opt); err == nil {
+		t.Error("Expected error when no user is specified")
+	}
+}
+
+func TestNoKeyFile(t *testing.T) {
+	opt := baseOpt
+	opt.KubeConfig = ""
+	opt.KeyFile = ""
+	if _, err := NewExecutor(&opt); err == nil {
+		t.Error("Expected error when no key file is specified")
+	}
+}
