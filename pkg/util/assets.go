8ae7dda427f10a54bad66de1565817159db19052
Alex de Wergifosse <adewergifosse@gmail.com>
2019-08-06 11:46:47 +1200

    Add functional tests for application && Fix concurrent read of asset

--- pkg/util/assets.go
+++ pkg/util/assets.go
@@ -21,21 +21,17 @@ import (
 
 var once sync.Once
 
-var staticFiles map[string]string
+var assets *rice.Box
 
 func GetStaticFile(filename string) string {
 
 	once.Do(func() {
-		staticFiles = make(map[string]string)
+		var err error
+		assets, err = rice.FindBox("../../assets")
+		Check(err)
 	})
 
-	fileContent, ok := staticFiles[filename]
-	if !ok {
-		assets, err := rice.FindBox("../../assets")
-		Check(err)
-		fileContent, err = assets.String(filename)
-		Check(err)
-		staticFiles[filename] = fileContent
-	}
+	fileContent, err := assets.String(filename)
+	Check(err)
 	return fileContent
 }
