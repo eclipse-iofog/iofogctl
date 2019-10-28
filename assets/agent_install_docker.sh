fcd1d61e80f6572e4193338547b17ce95463f2f3
Pixcell <alex@edgeworx.io>
2019-10-29 12:21:14 +1300

    Update overlay if docker already installed (#44)

--- assets/agent_install_docker.sh
+++ assets/agent_install_docker.sh
@@ -20,6 +20,24 @@ start_docker() {
 	set -e
 }
 
+do_configure_overlay() {
+	echo "# Configuring /etc/systemd/system/docker.service.d/overlay.conf..."
+	if [ "$lsb_dist" = "raspbian" ] || [ "$(uname -m)" = "armv7l" ] || [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "armv8" ]; then
+		if [ ! -d "/etc/systemd/system/docker.service.d" ]; then
+			$sh_c "mkdir -p /etc/systemd/system/docker.service.d"
+		fi
+		if [ -f "/etc/systemd/system/docker.service.d/overlay.conf" ] && ! grep -Fxq "ExecStart=/usr/bin/dockerd --storage-driver overlay -H unix:// -H tcp://127.0.0.1:2375" "/etc/systemd/system/docker.service.d/overlay.conf"; then
+			$sh_c 'echo "ExecStart=/usr/bin/dockerd --storage-driver overlay -H unix:// -H tcp://127.0.0.1:2375" >> /etc/systemd/system/docker.service.d/overlay.conf'
+		elif [ ! -f "/etc/systemd/system/docker.service.d/overlay.conf" ]; then
+			$sh_c 'echo "[Service]" > /etc/systemd/system/docker.service.d/overlay.conf'
+			$sh_c 'echo "ExecStart=" >> /etc/systemd/system/docker.service.d/overlay.conf'
+			$sh_c 'echo "ExecStart=/usr/bin/dockerd --storage-driver overlay -H unix:// -H tcp://127.0.0.1:2375" >> /etc/systemd/system/docker.service.d/overlay.conf'
+		fi
+		$sh_c "systemctl daemon-reload"
+		$sh_c "service docker restart"
+	fi
+}
+
 do_install_docker() {
 	# Check that Docker 18.09.2 or greater is installed
 	if command_exists docker; then
@@ -27,6 +45,7 @@ do_install_docker() {
 		if [ "$docker_version" -ge 18090 ]; then
 			echo "# Docker $docker_version already installed"
 			start_docker
+			do_configure_overlay
 			return
 		fi
 	fi
@@ -38,17 +57,7 @@ do_install_docker() {
 		exit 1
 	fi
 	start_docker
-
-	if [ "$lsb_dist" = "raspbian" ] || [ "$(uname -m)" = "armv7l" ] || [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "armv8" ]; then
-		if [ ! -d "/etc/systemd/system/docker.service.d" ]; then
-			$sh_c "mkdir -p /etc/systemd/system/docker.service.d"
-		fi
-		$sh_c 'echo "[Service]" > /etc/systemd/system/docker.service.d/overlay.conf'
-		$sh_c 'echo "ExecStart=" >> /etc/systemd/system/docker.service.d/overlay.conf'
-		$sh_c 'echo "ExecStart=/usr/bin/dockerd --storage-driver overlay -H unix:// -H tcp://127.0.0.1:2375" >> /etc/systemd/system/docker.service.d/overlay.conf'
-		$sh_c "systemctl daemon-reload"
-		$sh_c "service docker restart"
-	fi
+	do_configure_overlay
 }
 
 . /tmp/agent_init.sh
