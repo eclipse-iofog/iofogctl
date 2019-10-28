bd82e8541affb3eadb16663466e04a18e69d8174
Alex de Wergifosse <alex@edgeworx.io>
2019-10-29 09:55:49 +1300

    Add describe agent-config

--- internal/describe/agent-config.go
+++ internal/describe/agent-config.go
@@ -14,46 +14,62 @@
 package describe
 
 import (
-	apps "github.com/eclipse-iofog/iofog-go-sdk/pkg/apps"
+	"strings"
+
 	"github.com/eclipse-iofog/iofogctl/internal"
 
 	"github.com/eclipse-iofog/iofogctl/internal/config"
 	"github.com/eclipse-iofog/iofogctl/pkg/util"
 )
 
-type agentExecutor struct {
+type agentConfigExecutor struct {
 	namespace string
 	name      string
 	filename  string
 }
 
-func newAgentExecutor(namespace, name, filename string) *agentExecutor {
-	a := &agentExecutor{}
+func newAgentConfigExecutor(namespace, name, filename string) *agentConfigExecutor {
+	a := &agentConfigExecutor{}
 	a.namespace = namespace
 	a.name = name
 	a.filename = filename
 	return a
 }
 
-func (exe *agentExecutor) GetName() string {
+func (exe *agentConfigExecutor) GetName() string {
 	return exe.name
 }
 
-func (exe *agentExecutor) Execute() error {
+func (exe *agentConfigExecutor) Execute() error {
 	// Get config
 	agent, err := config.GetAgent(exe.namespace, exe.name)
 	if err != nil {
 		return err
 	}
 
+	// Connect to controller
+	ctrl, err := internal.NewControllerClient(exe.namespace)
+	if err != nil {
+		return err
+	}
+
+	getAgentResponse, err := ctrl.GetAgentByID(agent.UUID)
+	if err != nil {
+		// The agents might not be provisioned with Controller
+		if strings.Contains(err.Error(), "NotFoundError") {
+			return util.NewInputError("Cannot describe an Agent that is not provisioned with the Controller in namespace " + exe.namespace)
+		}
+		return err
+	}
+
 	header := config.Header{
 		APIVersion: internal.LatestAPIVersion,
-		Kind:       apps.AgentKind,
+		Kind:       config.AgentConfigKind,
 		Metadata: config.HeaderMetadata{
 			Namespace: exe.namespace,
 			Name:      exe.name,
 		},
-		Spec: agent,
+		Spec: getAgentResponse,
 	}
 
 	if exe.filename == "" {
