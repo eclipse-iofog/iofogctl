5f5cf2a5b452afb2e4262601a10b54747f63e005
Alex de Wergifosse <alex@edgeworx.io>
2019-12-11 17:43:43 +1300

    Add set -x to debug all ran commands

--- test/functions.bash
+++ test/functions.bash
@@ -520,10 +520,10 @@ function checkRenamedApplication() {
   OLDNAME=$1
   NEWNAME=$2
   NAMESPACE=$3
-  echo "Should be empty - iofogctl -n ${NAMESPACE} -v get applications | awk '{print $1}' | grep ${OLDNAME}:"
+  echo "Should be empty - iofogctl -n ${NAMESPACE} -v get applications | awk '{print \$1}' | grep ${OLDNAME}:"
   iofogctl -n ${NAMESPACE} -v get applications | awk '{print $1}' | grep ${OLDNAME}
 
-  echo "Should not be empty - iofogctl -n ${NAMESPACE} -v get applications | awk '{print $1}' | grep ${NEWNAME}"
+  echo "Should not be empty - iofogctl -n ${NAMESPACE} -v get applications | awk '{print \$1}' | grep ${NEWNAME}"
   iofogctl -n ${NAMESPACE} -v get applications | awk '{print $1}' | grep ${NEWNAME}
 
   [[ -z $(iofogctl -n ${NAMESPACE} -v get applications | awk '{print $1}' | grep ${OLDNAME}) ]]
