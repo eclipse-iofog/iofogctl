549d4680cc4b65bb2a78f84878d065007c84e5d0
Alex de Wergifosse <alex@edgeworx.io>
2020-02-27 09:57:09 +1300

    Move delete remote agent method

--- internal/delete/agent/execute.go
+++ internal/delete/agent/execute.go
@@ -19,7 +19,6 @@ import (
 	"github.com/eclipse-iofog/iofogctl/internal"
 	"github.com/eclipse-iofog/iofogctl/internal/config"
 	"github.com/eclipse-iofog/iofogctl/internal/execute"
-	"github.com/eclipse-iofog/iofogctl/pkg/iofog/install"
 	"github.com/eclipse-iofog/iofogctl/pkg/util"
 )
 
@@ -94,25 +93,4 @@ func (exe executor) Execute() error {
 	}
 
 	return nil
-}
-
-func (exe executor) deleteRemoteAgent(agent config.Agent) (err error) {
-	// Stop and remove the Agent process on remote server
-	if agent.Host == "" || agent.SSH.User == "" || agent.SSH.KeyFile == "" || agent.SSH.Port == 0 {
-		util.PrintNotify("Could not stop daemon for Agent " + agent.Name + ". SSH details missing from local cofiguration. Use configure command to add SSH details.")
-	} else {
-		sshAgent := install.NewRemoteAgent(
-			agent.SSH.User,
-			agent.Host,
-			agent.SSH.Port,
-			agent.SSH.KeyFile,
-			agent.Name,
-			agent.UUID,
-			nil,
-			exe.namespace)
-		if err = sshAgent.Uninstall(); err != nil {
-			util.PrintNotify(fmt.Sprintf("Failed to stop daemon on Agent %s. %s", agent.Name, err.Error()))
-		}
-	}
-	return
 }
--- internal/delete/agent/remote.go
+++ internal/delete/agent/remote.go
@@ -15,38 +15,14 @@ package deleteagent
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/eclipse-iofog/iofogctl/internal"
 	"github.com/eclipse-iofog/iofogctl/internal/config"
 	"github.com/eclipse-iofog/iofogctl/pkg/iofog/install"
 	"github.com/eclipse-iofog/iofogctl/pkg/util"
 )
 
-type remoteExecutor struct {
-	namespace string
-	name      string
-}
-
-func newRemoteExecutor(namespace, name string) *remoteExecutor {
-	exe := &remoteExecutor{}
-	exe.namespace = namespace
-	exe.name = name
-	return exe
-}
-
-func (exe *remoteExecutor) GetName() string {
-	return exe.name
-}
-
-func (exe *remoteExecutor) Execute() error {
-	// Check the agent exists
-	agent, err := config.GetAgent(exe.namespace, exe.name)
-	if err != nil {
-		return err
-	}
-
-	// Stop the Agent process on remote server
+func (exe executor) deleteRemoteAgent(agent config.Agent) (err error) {
+	// Stop and remove the Agent process on remote server
 	if agent.Host == "" || agent.SSH.User == "" || agent.SSH.KeyFile == "" || agent.SSH.Port == 0 {
 		util.PrintNotify("Could not stop daemon for Agent " + agent.Name + ". SSH details missing from local cofiguration. Use configure command to add SSH details.")
 	} else {
@@ -59,26 +35,9 @@ func (exe *remoteExecutor) Execute() error {
 			agent.UUID,
 			nil,
 			exe.namespace)
-		if err = sshAgent.Stop(); err != nil {
+		if err = sshAgent.Uninstall(); err != nil {
 			util.PrintNotify(fmt.Sprintf("Failed to stop daemon on Agent %s. %s", agent.Name, err.Error()))
 		}
 	}
-
-	ctrl, err := internal.NewControllerClient(exe.namespace)
-	// If controller exists, deprovision the agent
-	if err == nil {
-		// Perform deletion of Agent through Controller
-		if err = ctrl.DeleteAgent(agent.UUID); err != nil {
-			if !strings.Contains(err.Error(), "NotFoundError") {
-				return err
-			}
-		}
-	}
-
-	// Update config
-	if err = config.DeleteAgent(exe.namespace, exe.name); err != nil {
-		return err
-	}
-
-	return nil
+	return
 }
