ad8ebf27d307e574ab07220bf63eb300c0bab561
Alex de Wergifosse <alex@edgeworx.io>
2020-03-02 17:35:58 +1300

    Edit test/run preprocess

--- test/run.bash
+++ test/run.bash
@@ -14,14 +14,13 @@ function preprocess() {
         return 0
     fi
     while IFS= read -r GREP_LINE; do
-        grep -B 99999 "${GREP_LINE}" $1 > ${1}_temp
-        echo "GREP_LINE=$GREP_LINE"
+        grep -B 999999 "${GREP_LINE}" $1 > ${1}_temp
         local FILE_TO_LOAD=$(echo $GREP_LINE | awk '{print $3}')
-        echo "FILE_TO_LOAD=$FILE_TO_LOAD"
+        preprocess $FILE_TO_LOAD
         echo "" >> ${1}_temp
         cat $FILE_TO_LOAD >> ${1}_temp
         echo "" >> ${1}_temp
-        grep -A 9999 "${GREP_LINE}" $1 >> ${1}_temp
+        grep -A 999999 "${GREP_LINE}" $1 >> ${1}_temp
         mv ${1}_temp $1
     done <<< "$TO_LOAD"
 }
@@ -31,6 +30,5 @@ for TEST in "$@"; do
     TEST_FILE="test/tmp.bats"
     cp "test/$TEST.bats" $TEST_FILE
     preprocess $TEST_FILE
-    echo "Testing"
     bats $TEST_FILE
 done
