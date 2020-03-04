d015e0d79cd98f46dbe85a16337bddd2b9cd5505
SergeRadinovich <serge@edgeworx.io>
2020-03-04 13:18:05 +1300

    Check status before trying to jq response from api in hitMsvcEndpoint

--- test/functions.bash
+++ test/functions.bash
@@ -606,7 +606,7 @@ function hitMsvcEndpoint() {
   IP="$1"
   ITER=0
   SUCC=1
-  while [ $SUCC -ne 0 ] && [ $ITER -lt 6 ]; do
+  while [ $SUCC -ne 0 ] && [ $ITER -lt 12 ]; do
     sleep 10
     run curl -s --max-time 120 http://${IP}:5000/api/raw
     SUCC=$status
@@ -616,11 +616,14 @@ function hitMsvcEndpoint() {
 
   ITER=0
   COUNT=0
-  while [ $COUNT -eq 0 ] && [ $ITER -lt 6 ]; do
+  while [ $COUNT -eq 0 ] && [ $ITER -lt 12 ]; do
     sleep 10
     RET=$(curl -s --max-time 120 http://${IP}:5000/api/raw)
     echo "$RET"
-    COUNT=$(echo "$RET" | jq '. | length')
+    run echo "$RET" | jq '. | length'
+    if [ $status -eq 0 ]; then
+      COUNT=$(echo "$RET" | jq '. | length')
+    fi
     ITER=$((ITER+1))
   done
   [ $COUNT -gt 0 ]
