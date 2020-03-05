1a96d4fb7fdf434af6335664c8a1d4993d6d7a5e
SergeRadinovich <serge@edgeworx.io>
2020-03-05 15:43:56 +1300

    Update clean.bash to use env.sh instead of take arg for ns

--- test/clean.bash
+++ test/clean.bash
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-    echo "Please specify a namespace to clean as an argument to this script"
-    exit 1
-fi
+. test/conf/env.sh
 
-NAMESPACE="$1"
 echo "Cleaning namespace $NAMESPACE"
 
 iofogctl delete all -n "$NAMESPACE" -v
