5df38218a5e07e3016be8b436797bfb017f71441
SergeRadinovich <serge@edgeworx.io>
2020-04-08 13:29:54 +1200

    Increase timeout when installing Agent

--- assets/agent_wait.sh
+++ assets/agent_wait.sh
@@ -6,7 +6,7 @@ STATUS=""
 ITER=0
 while [ "$STATUS" != "RUNNING" ] ; do
     ITER=$((ITER+1))
-    if [ "$ITER" -gt 30 ]; then
+    if [ "$ITER" -gt 60 ]; then
         echo 'Timed out waiting for Agent to be RUNNING'
         exit 1
     fi
