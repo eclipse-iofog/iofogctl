a8cf86a097103bc8ed00562906c42072e4986343
sergerad <serge.radinovich@gmail.com>
2021-03-15 11:20:19 +1300

    Pin enterprise controller version

--- test/clean.bash
+++ test/clean.bash
@@ -4,12 +4,7 @@
 
 echo "Cleaning namespace $NAMESPACE"
 
-iofogctl delete all -n "$NAMESPACE" -v
-kubectl delete controlplanes/iofog -n "$NAMESPACE"
-kubectl delete all --all -n "$NAMESPACE"
-kubectl delete clusterrolebinding "${NAMESPACE}-iofog-operator"
 kubectl delete ns "$NAMESPACE"
 iofogctl disconnect -n "$NAMESPACE" -v
-iofogctl delete namespace "$NAMESPACE" -v
 
 exit 0
