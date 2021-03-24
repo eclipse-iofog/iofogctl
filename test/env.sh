91f0cdcaf564a96600b14e058b508ab358b5a10a
sergerad <serge.radinovich@gmail.com>
2021-03-25 10:47:51 +1300

    v3.0.0-alpha1

--- test/env.sh
+++ test/env.sh
@@ -24,24 +24,24 @@ export TEST_KUBE_CONFIG="~/.kube/config"
 export KEY_FILE="~/.ssh/id_rsa"
 
 # Images of ioFog services deployed on Kubernetes cluster or local deploy
-export CONTROLLER_IMAGE='gcr.io/focal-freedom-236620/controller:develop'
-export AGENT_IMAGE='gcr.io/focal-freedom-236620/agent:develop'
-export PORT_MANAGER_IMAGE='gcr.io/focal-freedom-236620/port-manager:develop'
-export ROUTER_IMAGE='gcr.io/focal-freedom-236620/router:develop'
-export ROUTER_ARM_IMAGE='gcr.io/focal-freedom-236620/router-arm:develop'
-export PROXY_IMAGE='gcr.io/focal-freedom-236620/proxy:develop'
-export PROXY_ARM_IMAGE='gcr.io/focal-freedom-236620/proxy-arm:develop'
-export OPERATOR_IMAGE='gcr.io/focal-freedom-236620/operator:develop'
-export KUBELET_IMAGE='gcr.io/focal-freedom-236620/kubelet:develop'
+export CONTROLLER_IMAGE='gcr.io/focal-freedom-236620/controller:v3.0.0-alpha1'
+export AGENT_IMAGE='gcr.io/focal-freedom-236620/agent:v3.0.0-alpha1'
+export PORT_MANAGER_IMAGE='gcr.io/focal-freedom-236620/port-manager:v3.0.0-alpha1'
+export ROUTER_IMAGE='gcr.io/focal-freedom-236620/router:v3.0.0-alpha1'
+export ROUTER_ARM_IMAGE='gcr.io/focal-freedom-236620/router-arm:v3.0.0-alpha1'
+export PROXY_IMAGE='gcr.io/focal-freedom-236620/proxy:v3.0.0-alpha1'
+export PROXY_ARM_IMAGE='gcr.io/focal-freedom-236620/proxy-arm:v3.0.0-alpha1'
+export OPERATOR_IMAGE='gcr.io/focal-freedom-236620/operator:v3.0.0-alpha1'
+export KUBELET_IMAGE='gcr.io/focal-freedom-236620/kubelet:v3.0.0-alpha1'
 
 # Controller version for vanilla deploys
-export CONTROLLER_VANILLA_VERSION='0.0.0-dev'
+export CONTROLLER_VANILLA_VERSION='3.0.0-alpha1'
 export CONTROLLER_REPO='iofog/iofog-controller-snapshots'
 # Token to access develop versions of Controller
 export CONTROLLER_PACKAGE_CLOUD_TOKEN=""
 
 # Agent version for vanilla deploys
-export AGENT_VANILLA_VERSION='0.0.0-dev'
+export AGENT_VANILLA_VERSION='3.0.0-alpha1'
 export AGENT_REPO='iofog/iofog-agent-snapshots'
 # Token to access develop versions of Agent
 export AGENT_PACKAGE_CLOUD_TOKEN=""
