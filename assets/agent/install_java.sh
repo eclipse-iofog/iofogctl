aa7a155ff65ba7b22e7cf3c4201970adcbc83be3
Neha Naithani <35979902+NehaNaithani@users.noreply.github.com>
2021-08-19 15:50:07 +1200

    Upgraded java 8 to openjdk-11 (#162)

--- assets/agent/install_java.sh
+++ assets/agent/install_java.sh
@@ -2,35 +2,35 @@
 set -x
 set -e
 
-java8_version=0
+java_major_version=0
+java_minor_version=0
 do_check_install() {
 	if command_exists java; then
-        java8_version="$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | grep 1.8 | cut -d'_' -f 2)"
+        java_major_version="$(java --version | head -n1 | awk '{print $2}' | cut -d. -f1)"
+        java_minor_version="$(java --version | head -n1 | awk '{print $2}' | cut -d. -f2)"
 	fi
-	if [ "$java8_version" -ge "181" ]; then
-		echo "Java $java8_version already installed."
+	if [ "$java_major_version" -ge "11" ]  && [ "$java_minor_version" -ge "0" ]; then
+		echo "Java $java_major_version.$java_minor_version  already installed."
 		exit 0
 	fi
 }
 
 do_install_java() {
-	echo "# Installing java 8..."
+	echo "# Installing java 11..."
 	echo ""
 	os_arch=$(getconf LONG_BIT)
 	is_arm=""
 	if [ "$lsb_dist" = "raspbian" ] || [ "$(uname -m)" = "armv7l" ] || [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "armv8" ]; then
 		is_arm="-arm"
 	fi
-	cd /opt/
-	$sh_c 'wget --no-check-certificate '"https://storage.googleapis.com/edgeworx/downloads/jdk/jdk-8u211$is_arm-$os_arch.tar.gz"''
-	$sh_c "tar xzf jdk-8u211$is_arm-$os_arch.tar.gz"
-	cd /opt/jdk1.8.0_211/	
 	case "$lsb_dist" in
 		debian|raspbian|ubuntu|mendel)
-			$sh_c "update-alternatives --install /usr/bin/java java /opt/jdk1.8.0_211/bin/java 1100"
+		  $sh_c "add-apt-repository ppa:openjdk-r/ppa -y"
+      $sh_c "apt-get update"
+      $sh_c "apt install -y openjdk-11-jdk"
 			;;		
 		fedora|centos)
-			$sh_c "alternatives --install /usr/bin/java java /opt/jdk1.8.0_211/bin/java 4"
+      $sh_c "yum install -y openjdk-11-jdk"
 			;;
 	esac
 }
@@ -51,10 +51,6 @@ do_install_deps() {
 		sleep 5
 		iter=$((iter+1))
 	done
-
-	if [ -z $(command -v wget) ]; then
-		$sh_c "$installer install -y wget"
-	fi
 }
 
 . /etc/iofog/agent/init.sh
