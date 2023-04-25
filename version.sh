7323af0e4771b04b633734fdfad7ac8e99610e87
NehaNaithani <neha@edgeworx.io>
2023-04-25 13:22:44 +0530

    Added env variables for tests

--- version.sh
+++ version.sh
@@ -7,3 +7,5 @@ PATCH=$(echo "$LATEST_TAG" | tr -d "v" | sed "s|-.*||" | sed -E "s|.\..\.(.)|\1|
 SUFFIX_TAG=$(echo "$LATEST_TAG" | sed "s|v$MAJOR\.$MINOR\.$PATCH||")
 SUFFIX_DEV="-dev"
 SUFFIX="$([ -z "$(git tag --points-at HEAD)" ] && echo "$SUFFIX_DEV" || echo "$SUFFIX_TAG")"
+echo $SUFFIX
+echo $SUFFIX_TAG
